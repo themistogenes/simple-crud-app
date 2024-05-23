0d14145c797aacc2039bb671706c58424ff75a64
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-22 21:24:30 -0400

    create api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 
 const app = express();
+app.use(express.json()); // allows JSON in a POST request
 
-mongoose.connect('mongodb+srv://jalimaras:secret1819@cluster0.jyhl7j0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://jalimaras:secret1819@cluster0.jyhl7j0.mongodb.net/simple-crud-app-db?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
     console.log('Connected to DB!');
     app.listen(3000, () => {
@@ -16,4 +18,13 @@ mongoose.connect('mongodb+srv://jalimaras:secret1819@cluster0.jyhl7j0.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
+});
+
+app.post('/api/products', async (req, res) => {
+try {
+  const product = await Product.create(req.body);
+  res.status(200).json(product);
+} catch (error) {
+  res.status(500).json({message: error.message});
+}
 });
